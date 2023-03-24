e36d3050bfad1794b5f1380ca92491181bea8680
SayrahSyl <sarahinwang@gmail.com>
2023-03-23 21:55:25 -0700

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 
 /**
@@ -17,9 +18,7 @@ int main(void)
 /* putchar(i);*/
 /*}*/
 
-printf("Infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
-return (0);
-
+	return (0);
 }
-
