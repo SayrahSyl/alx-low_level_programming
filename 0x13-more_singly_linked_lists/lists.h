6e4aa67aab29b3e4e55bf93e12b8240ccd676448
SayrahSyl <sarahinwang@gmail.com>
2023-05-02 17:26:01 -0700

    task 4

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -23,5 +23,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+void free_listint(listint_t *head);
 
 #endif
